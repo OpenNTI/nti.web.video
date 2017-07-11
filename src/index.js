62be0beb2f2948bf31d4d158cf2661e92629d35f
Andrew Ligon <andrew.ligon@nextthought.com>
2017-07-10 19:02:07 -0500

    Export player state constants

--- src/index.js
+++ src/index.js
@@ -3,6 +3,7 @@ import Editor, {EmbedInput} from './editor';
 import ComponentRaw from './Video';
 import Component from './VideoWithAnalytics';
 import Placeholder from './Placeholder';
+import {UNSTARTED, ENDED, PLAYING, PAUSED, BUFFERING, CUED} from './Constants';
 
 export default ComponentRaw;
 
@@ -14,5 +15,11 @@ export {
 	Component,
 	Placeholder,
 	Editor,
-	EmbedInput
+	EmbedInput,
+	UNSTARTED,
+	ENDED,
+	PLAYING,
+	PAUSED,
+	BUFFERING,
+	CUED
 };
