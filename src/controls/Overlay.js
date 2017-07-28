d60326eea49e3a4ddcce4d21dc7c3f9c36c27ef4
Andrew Ligon <andrew.ligon@nextthought.com>
2017-07-27 23:27:01 -0500

    Hide the controls when not active

--- src/controls/Overlay.js
+++ src/controls/Overlay.js
@@ -7,12 +7,35 @@ import LowerControls from './LowerControls';
 import Mask from './Mask';
 import UpperControls from './UpperControls';
 
+const HIDE_ON_INACTIVE = 5000;
+const HIDE_ON_LEAVE = 3000;
+
 export default class VideoControlsOverlay extends React.Component {
 	static propTypes = {
 		className: PropTypes.string,
 		videoState: PropTypes.object,
 		onPlay: PropTypes.func,
 		onPause: PropTypes.func
+	}
+
+	state = {
+		showControls: false
+	}
+
+
+	startHideTimer (timeout) {
+		this.stopHideTimer();
+
+		this.hideControlsTimeout = setTimeout(() => {
+			this.setState({
+				showControls: false
+			});
+		}, timeout);
+	}
+
+
+	stopHideTimer () {
+		clearTimeout(this.hideControlsTimeout);
 	}
 
 
@@ -27,15 +50,37 @@ export default class VideoControlsOverlay extends React.Component {
 		}
 	}
 
+	onMouseOver = () => {
+		this.stopHideTimer();
+
+		this.setState({showControls: true});
+	}
+
+
+	onMouseOut = () => {
+		this.startHideTimer(HIDE_ON_LEAVE);
+	}
+
+
+	onMouseMove = () => {
+		this.stopHideTimer();
+
+		this.setState({showControls: true}, () => {
+			this.startHideTimer(HIDE_ON_INACTIVE);
+		});
+	}
+
 	render () {
 		const {videoState, className, ...otherProps} = this.props;
+		const {showControls} = this.state;
 		const interacted = hasInteractedWithVideo(videoState);
+		const cls = cx('video-controls-overlay', className, {'show-controls': showControls && interacted});
 
 		return (
-			<div className={cx('video-controls-overlay', className)} onClick={this.onClick}>
+			<div className={cls} onClick={this.onClick} onMouseOver={this.onMouseOver} onMouseOut={this.onMouseOut} onMouseMove={this.onMouseMove} >
 				{!interacted && (<Mask {...otherProps} />)}
-				{interacted && (<UpperControls className="overlay-upper-controls" videoState={videoState} {...otherProps} />)}
-				{interacted && (<LowerControls className="overlay-lower-controls" videoState={videoState} {...otherProps} />)}
+				<UpperControls className="overlay-upper-controls" videoState={videoState} {...otherProps} />
+				<LowerControls className="overlay-lower-controls" videoState={videoState} {...otherProps} />
 			</div>
 		);
 	}
