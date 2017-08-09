7f7544cd7e947751022f55c6f937585c6a4e6680
Andrew Ligon <andrew.ligon@nextthought.com>
2017-08-09 18:43:21 -0500

    debugger;

--- src/controls/utils/get-formatted-current-time.js
+++ src/controls/utils/get-formatted-current-time.js
@@ -1,7 +1,6 @@
 import formatTime from './format-time';
 
 export default function getFormattedCurrentTime (videoState) {
-	debugger;
 	const {currentTime} = videoState || {};
 
 	return formatTime(currentTime || 0);
