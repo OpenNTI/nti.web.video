71108c88417ee9fe2f1abf259d7fb0e026fb0074
Andrew Ligon <andrew.ligon@nextthought.com>
2017-10-11 17:18:14 -0500

    If all the sources fail to load show a message

--- src/controls/Mask.jsx
+++ src/controls/Mask.jsx
@@ -1,19 +1,35 @@
 import React from 'react';
 import PropTypes from 'prop-types';
+import cx from 'classnames';
+import {scoped} from 'nti-lib-locale';
 import {Loading} from 'nti-web-commons';
+
+const DEFAULT_TEXT = {
+	unableToPlay: 'Unable to play video'
+};
+
+const t = scoped('nti-video.controls.mask', DEFAULT_TEXT);
 
 VideoControlsMask.propTypes = {
 	poster: PropTypes.string,
 	buffering: PropTypes.bool,
-	interacted: PropTypes.bool
+	interacted: PropTypes.bool,
+	hasSources: PropTypes.bool
 };
-export default function VideoControlsMask ({poster, buffering, interacted }) {
-	const style = poster ? {backgroundImage: `url(${poster})`} : {backgroundColor: 'transparent'};
+export default function VideoControlsMask ({poster, buffering, interacted, hasSources }) {
+	const cls = cx('video-controls-mask', {'no-sources': !hasSources});
+	const style = {};
 
+	if (poster && hasSources) {
+		style.backgroundImage = `url(${poster})`;
+	} else if (hasSources) {
+		style.backgroundColor = 'transparent';
+	}
 	return (
-		<div className="video-controls-mask" style={style}>
-			{!interacted && (<span className="play-button" />)}
-			{buffering && interacted && (<span className="buffer"><Loading.Spinner white size="50px" /></span>)}
+		<div className={cls} style={style}>
+			{!hasSources && (<span className="unable-to-play">{t('unableToPlay')}</span>)}
+			{!interacted && hasSources && (<span className="play-button" />)}
+			{buffering && interacted && hasSources && (<span className="buffer"><Loading.Spinner white size="50px" /></span>)}
 		</div>
 	);
 }
