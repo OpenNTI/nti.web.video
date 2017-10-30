367b3bb82e54663dec6af72d1b8880368f2d930e
Jonathan Grimes <jsg2021@gmail.com>
2017-10-30 18:04:18 -0500

    Move these tests out. They aren't testing the correct thing anyways.

--- src/services/__test__/mockMetaData.js
+++
@@ -1,6 +0,0 @@
-export default {
-	'description': 'A short film about winter.\n\nSnow is particularly joyful in how it transforms everything it coversThe brightness of its white forces the iris in your eye to shut way down, and suddenly everything that isn’t snow is in silhouette or defined by shadow. The world becomes a high-contrast graphic representation of itself. This distillation of form allowed me to focus on painting pure motion in relation to the winter landscape.\n\nThis film is composed of approximately 2,250 watercolor paintings on paper. It is  a kind of winter companion to my film “L’Eau Life,” although here the water is frozen and therefore much easier to paint — because painting snow mostly means leaving the paper white.\n\nThe music is by Shay Lynch, and wonderfully captures the endless cascade of the montage while giving the film a kind of emotional elegance in spite of its vaudevillian pratfalls.This film and essay originally appeared in my blog The Animated Life in the New York Times, Opinion.',
-	'title': 'white Out',
-	'thumbnail': 'https://i.vimeocdn.com/video/509776556_295x166.jpg',
-	'poster': 'https://i.vimeocdn.com/video/509776556_295x166.jpg'
-};
