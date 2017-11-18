560f88f52bd242a630933bbeccfbe7b7d3245a81
Andrew Ligon <andrew.ligon@nextthought.com>
2017-11-18 16:52:07 -0600

    NTI-4495 handle a source with multiple sources

--- src/services/utils/can-play-type.js
+++ src/services/utils/can-play-type.js
@@ -1,7 +1,21 @@
 let video = null;
 
-export default function canPlayType (type) {
+function canPlay (type) {
 	video = video || (document && document.createElement && document.createElement('video'));
 
 	return !type || !video || !video.canPlayType || video.canPlayType(type);
+}
+
+export default function canPlayType (type) {
+	if (!Array.isArray(type)) {
+		type = [type];
+	}
+
+	for (let t of type) {
+		if (canPlay(t)) {
+			return true;
+		}
+	}
+
+	return false;
 }
