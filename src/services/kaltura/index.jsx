7ef865dc0637dc309b1102c9555335d3e705df21
Cory Jones <cory.jones@nextthought.com>
2018-01-11 11:20:53 -0600

    NTI-4840 Only set crossorigin if we have no custom tracks

--- src/services/kaltura/index.jsx
+++ src/services/kaltura/index.jsx
@@ -136,7 +136,7 @@ export default class KalturaVideo extends React.Component {
 		 */
 		source: PropTypes.any.isRequired,
 		tracks: PropTypes.array,
-		
+
 		autoPlay: PropTypes.bool,
 		deferred: PropTypes.bool,
 
@@ -263,13 +263,14 @@ export default class KalturaVideo extends React.Component {
 			poster,
 			source: void 0,
 			sources,
+			crossorigin: tracks && tracks.length > 0 ? false : true,
 			tracks: tracks && tracks.length > 0 ? tracks : defaultTracks
 		};
 
 		return (
 			<div className="kaltura-wrapper">
 				{!sourcesLoaded && (<Loading />)}
-				{sourcesLoaded && (<Video {...videoProps} ref={this.attachRef} allowNormalTranscripts crossOrigin />)}
+				{sourcesLoaded && (<Video {...videoProps} ref={this.attachRef} allowNormalTranscripts />)}
 			</div>
 		);
 	}
