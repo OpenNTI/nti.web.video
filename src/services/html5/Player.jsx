22cd396dd9bdb9d9caa507b5385fbc2649522441
Jonathan Grimes <jsg2021@gmail.com>
2018-04-26 20:42:39 -0500

    dont re-fallback

--- src/services/html5/Player.jsx
+++ src/services/html5/Player.jsx
@@ -544,7 +544,7 @@ export default class HTML5Video extends React.Component {
 	onSourceError = (e) => {
 		e.stopPropagation();
 
-		if (HLS.isSupported() && e.target.type === HLS_TYPE) {
+		if (HLS.isSupported() && e.target.type === HLS_TYPE && !this.hls) {
 			events.error('HLS Supported, got error for HLS source: %o\nEvent: %o', e.target, e.nativeEvent);
 			this.polyfillHLS(e.target.src);
 			return;
