d275f4c39f35a1e7120d38d4ea18c18e23a90094
Andrew Ligon <andrew.ligon@nextthought.com>
2020-01-06 17:40:32 -0600

    NTI-8881 support more wistia url formats

--- src/editor/utils/parse-embed-code.js
+++ src/editor/utils/parse-embed-code.js
@@ -1,17 +1,53 @@
-const WistiaExternalsRegex = /wistia\.com\/assets\/external/;
-const WistiaIdRegex = /embed\/medias\/([^./]*)\.jsonp/;
+const WistiaRegex = {
+	async: {
+		external: /wistia\.com\/assets\/external/,
+		id: /embed\/medias\/([^./]*)\.jsonp/
+	},
+	media: {
+		url: /wistia\.com\/medias/,
+		id: /wistia\.com\/medias\/([^.?/]*)/
+	},
+	iframeEmbed: {
+		url: /wistia\.net\/embed\/iframe/,
+		id: /wistia\.net\/embed\/iframe\/.([^.?/]*)/
+	}
+};
+
+const normalizeWistia = (id) => `https://fast.wistia.com/embed/iframe/${id}`;
 
 const CustomParsers = [
 	//wistia async embed
 	(input) => {
-		if (!WistiaExternalsRegex.test(input)) { return null; }
+		if (!WistiaRegex.async.external.test(input)) { return null; }
 
-		const matches = input.match(WistiaIdRegex);
+		const matches = input.match(WistiaRegex.async.id);
 		const id = matches[1];
 
 		if (!id) { return null; }
 
-		return `https://fast.wistia.com/embed/iframe/${id}`;
+		return normalizeWistia(id);
+	},
+	//wistia media url
+	(input) => {
+		if (!WistiaRegex.media.url.test(input)) { return null; }
+
+		const matches = input.match(WistiaRegex.media.id);
+		const id = matches[1];
+
+		if (!id) { return null; }
+
+		return normalizeWistia(id);
+	},
+	//wistia iframe url
+	(input) => {
+		if (!WistiaRegex.iframeEmbed.url.test(input)) { return null; }
+
+		const matches = input.match(WistiaRegex.iframeEmbed.id);
+		const id = matches[1];
+
+		if (!id) { return null; }
+
+		return normalizeWistia(id);
 	}
 ];
 
