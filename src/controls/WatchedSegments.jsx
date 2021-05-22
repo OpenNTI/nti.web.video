5d8a695538ae865bcb90219becea958065a13e36
Jonathan Grimes <jsg2021@users.noreply.github.com>
2021-05-21 19:35:26 -0500

    lint

--- src/controls/WatchedSegments.jsx
+++ src/controls/WatchedSegments.jsx
@@ -1,7 +1,7 @@
 import React from 'react';
 import PropTypes from 'prop-types';
 
-import { Hooks, Loading } from '@nti/web-commons';
+import { Hooks } from '@nti/web-commons';
 
 import { usePlayer } from '../Context';
 
