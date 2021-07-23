645333162c206ab5c30a9eddf159fcb5782336fe
Mahmoud Mousa Hamad <mahmoudmousahamad@gmail.com>
2021-07-23 13:07:19 -0500

    [NTI-10956] Edit Hook

--- src/hooks/use-video-completion.js
+++ src/hooks/use-video-completion.js
@@ -1,38 +1,25 @@
-import { useResolver } from '@nti/web-commons';
-
-import { usePlayer, useDuration } from '../Context';
+import { usePlayer, useDuration, useCurrentTime } from '../Context';
 
 /**
  * Returns whether the current video has been completed.
  *
- * @param {Array} segments
- * @returns {boolean}
+ * @returns {{watchedTilEnd: boolean, videoCompletable: boolean, videoCompleted: boolean}}
  */
-export default function useVideoCompletion(segments) {
+export default function useVideoCompletion() {
 	const duration = useDuration();
 	const player = usePlayer();
+	const time = useCurrentTime();
 	const video = player?.video;
 
-	const percentCompleted =
-		(100 *
-			segments?.reduce(
-				(acc, segment) =>
-					acc + (segment['data-end'] - segment['data-start']),
-				0
-			)) /
-		duration;
+	if (!duration) {
+		return {
+			videoCompletable: false,
+		};
+	}
 
-	const bin = percentCompleted - (percentCompleted % 5);
-
-	const resolver = useResolver(async () => {
-		if (!video) {
-			return;
-		}
-
-		await video?.refresh();
-
-		return video.isCompletable() && video.hasCompleted();
-	}, [player, duration, bin]);
-
-	return useResolver.isResolved(resolver) ? resolver : null;
+	return {
+		watchedTilEnd: time >= duration * 0.875, // Simplify calculation: 7/8 = 0.875
+		videoCompletable: video?.isCompletable(),
+		videoCompleted: video?.hasCompleted() && video?.completedSuccessfully(),
+	};
 }
