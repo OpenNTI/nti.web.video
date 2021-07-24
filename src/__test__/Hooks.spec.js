53f44535679d9c9d75c6288be27f6e8b364451ee
Mahmoud Mousa Hamad <mahmoudmousahamad@gmail.com>
2021-07-23 20:45:28 -0500

    [NTI-10956] Fix test

--- src/__test__/Hooks.spec.js
+++ src/__test__/Hooks.spec.js
@@ -5,32 +5,40 @@ import * as Commons from '@nti/web-commons';
 
 import * as Context from '../Context';
 import useVideoCompletion from '../hooks/use-video-completion';
+import { ENDED } from '../Constants';
 
 jest.mock('@nti/web-commons');
 
 beforeEach(() => {
 	Commons.useResolver.mockImplementation(fn => fn());
 	Commons.useResolver.isResolved.mockImplementation(() => true);
-
-	jest.spyOn(Context, 'useDuration').mockImplementation(() => 60);
 });
 
-test('useVideoCompletion Hook.', () => {
+test('useVideoCompletion Hook.', async () => {
 	const refresh = jest.fn();
 
-	jest.spyOn(Context, 'usePlayer').mockImplementation(() => {
-		return {
-			video: {
-				isCompletable: () => true,
-				hasCompleted: () => false,
-				refresh: refresh,
-			},
-		};
-	});
+	jest.spyOn(Context, 'useDuration').mockImplementation(() => 100);
+	jest.spyOn(Context, 'usePlayer').mockImplementation(() => ({
+		getPlayerState: () => ({ state: ENDED }),
+		video: {
+			isCompletable: () => true,
+			hasCompleted: () => true,
+			completedSuccessfully: () => true,
+			refresh: refresh,
+		},
+	}));
+	jest.spyOn(Context, 'useCurrentTime').mockImplementation(() => 90);
 
-	const segments = [{ 'data-end': 1, 'data-start': 0 }];
+	const hook = renderHook(() => useVideoCompletion());
 
-	renderHook(() => useVideoCompletion(segments));
+	hook.waitForNextUpdate();
+
+	const completion = await hook.result.current;
 
 	expect(refresh).toBeCalled();
+	expect(completion).toEqual({
+		watchedTilEnd: true,
+		videoCompletable: true,
+		videoCompleted: true,
+	});
 });
